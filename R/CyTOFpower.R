46501c87a29b9e8a3a641d11921b129902f3f288
Anne-Maud Ferreira <amferrei@Susans-MacBook-Pro-4.local>
2021-09-08 18:13:09 -0700

    Initial commit

---
+++ R/CyTOFpower.R
@@ -0,0 +1,10 @@
+#' CyTOFpower: R-package to predict the power of a CyTOF experiment
+#'
+#' This package provides functions and a shiny app to predict the power of a
+#' CyTOF experiment.
+#'
+#' @docType package
+#' @name CyTOFpower
+#'
+#' @import shiny
+NULL
