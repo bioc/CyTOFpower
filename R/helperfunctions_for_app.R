d3cb968d276a6666755ab5a8acb1ffd54474e93d
Anne-Maud Ferreira <amferrei@Susans-MacBook-Pro-4.local>
2021-09-09 16:47:36 -0700

    Add documentation Add .data$

--- R/helperfunctions_for_app.R
+++ R/helperfunctions_for_app.R
@@ -1,48 +1,71 @@
-# Compute power
-compute_pwr <- function(model_values, # Output of run_models()
-                        alpha = 0.05){ # Significance level
-  # Compute the power for each marker
+#' Compute power
+#'
+#' @details Compute the power based on the model values
+#'
+#' @param model_values data.frame, output of run_models()
+#' @param alpha numeric, significance level
+compute_pwr <- function(model_values,
+                        alpha = 0.05){
   model_values %>%
-    dplyr::filter(truth == 1) %>%
-    dplyr::group_by(model, marker_id) %>% #  rho, nb_donor, subject_effect, nb_DE_marker,  nb_cell_per_sample, total_nb_marker
-    dplyr::summarize(power = mean(p_adj < alpha))
+    dplyr::filter(.data$truth == 1) %>% # Filter the values with a true fold change
+    dplyr::group_by(.data$model, .data$marker_id) %>% # Group by models and marker ID
+    dplyr::summarize(power = mean(.data$p_adj < alpha)) # Compute the power
 }
 
-# Compute variance
+#' Compute variance
+#'
+#' @details Compute the observed variance in the data
+#'
+#' @param raw_data_lg data.frame, cells values in long format
 compute_variance <- function(raw_data_lg){
-  # Observed sample variance
+  # Sample variance
   obs_sample_variance <- raw_data_lg %>%
-    dplyr::group_by(i, donor_id) %>%
-    dplyr::summarize(variance = var(raw_intensity))
+    dplyr::group_by(.data$i, .data$donor_id) %>%
+    dplyr::summarize(variance = stats::var(.data$raw_intensity))
+  # Mean of the sample variance
   mean_obs_sample_variance <- obs_sample_variance %>%
-    dplyr::group_by(donor_id) %>%
-    dplyr::summarize(variance = round(mean(variance), digits = 2))
+    dplyr::group_by(.data$donor_id) %>%
+    dplyr::summarize(variance = round(mean(.data$variance), digits = 2))
 
   return(mean_obs_sample_variance)
 }
-# Compute variance
+
+#' Compute effect size
+#'
+#' @details Compute observed Cohen's effect size and observed fold change
+#'
+#' @param raw_data_lg data.frame, cells values in long format
 compute_effectsize <- function(raw_data_lg){
   # Effect size
   ## Mean
   mean_raw_data_summary_1 <- raw_data_lg %>%
-    dplyr::group_by(group_id, markers) %>%
-    dplyr::summarize(mean = mean(raw_intensity))
+    dplyr::group_by(.data$group_id, .data$markers) %>%
+    dplyr::summarize(mean = mean(.data$raw_intensity))
   ## Mean difference
   mean_raw_data_summary_1_wd <- mean_raw_data_summary_1 %>%
-    tidyr::pivot_wider(id_cols = markers, names_from = group_id, values_from = mean)
-  mean_diff_raw_data_summary_1_wd <- dplyr::summarize(mean_raw_data_summary_1_wd, mean_diff = B - A, markers)
+    tidyr::pivot_wider(id_cols = .data$markers,
+                       names_from = .data$group_id,
+                       values_from = .data$mean)
+  mean_diff_raw_data_summary_1_wd <- dplyr::summarize(mean_raw_data_summary_1_wd,
+                                                      mean_diff = .data$B - .data$A,
+                                                      .data$markers)
   ## Standard deviation
   sd_raw_data_summary_1 <- raw_data_lg %>%
-    dplyr::group_by(i, markers)  %>%
-    dplyr::summarize(standard_deviation = sd(raw_intensity))
+    dplyr::group_by(.data$i, .data$markers)  %>%
+    dplyr::summarize(standard_deviation = stats::sd(.data$raw_intensity))
   sd_mean_raw_data_summary_1 <- sd_raw_data_summary_1 %>%
-    dplyr::group_by(markers)  %>%
-    dplyr::summarize(mean_sd = mean(standard_deviation))
+    dplyr::group_by(.data$markers)  %>%
+    dplyr::summarize(mean_sd = mean(.data$standard_deviation))
   ## Combine
   cb_raw_data <- dplyr::left_join(mean_diff_raw_data_summary_1_wd, sd_mean_raw_data_summary_1)
-  eff_size_raw_data <- dplyr::summarize(cb_raw_data, effect_size = round(mean_diff / mean_sd, digits = 1), markers)
+  eff_size_raw_data <- dplyr::summarize(cb_raw_data,
+                                        effect_size = round(.data$mean_diff / .data$mean_sd, digits = 1),
+                                        .data$markers)
+
   # Fold Change
-  obs_fc <- dplyr::summarize(mean_raw_data_summary_1_wd, observed_FC = round(B / A, digits = 1), markers)
+  obs_fc <- dplyr::summarize(mean_raw_data_summary_1_wd,
+                             observed_FC = round(.data$B / .data$A, digits = 1),
+                             .data$markers)
 
   # Return
   res <- dplyr::left_join(eff_size_raw_data, obs_fc)
