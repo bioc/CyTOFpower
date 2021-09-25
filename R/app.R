7b1d3012bbebdc70b14fa0ee9683443bcd3217d0
Anne-Maud Ferreira <amferrei@stanford.edu>
2021-09-24 17:03:42 -0700

    sort the values of the precomputed datasets

--- R/app.R
+++ R/app.R
@@ -3,20 +3,20 @@ precomputed_data <- utils::read.table(system.file("extdata",
                                         "df_precomputed_datasets.txt",
                                         package = "CyTOFpower"))
 list_param_precomupteddataset <-list(
-  "nb_donor" = c(unique(precomputed_data$nb_donor), NA),
-  "total_nb_markers" = c(unique(precomputed_data$total_nb_markers), NA),
-  "nb_DEmarkers" = unique(precomputed_data$nb_DEmarkers),
-  "subject_effect" = c(unique(precomputed_data$subject_effect), NA),
-  "mu0" = c(unique(precomputed_data$mu0), NA),
-  "dispersion" = c(unique(precomputed_data$dispersion), NA),
-  "nb_cell_per_sample" = c(unique(precomputed_data$nb_cell_per_sample), NA)
+  "nb_donor" = c(sort(unique(precomputed_data$nb_donor)), NA),
+  "total_nb_markers" = c(sort(unique(precomputed_data$total_nb_markers)), NA),
+  "nb_DEmarkers" = sort(unique(precomputed_data$nb_DEmarkers)),
+  "subject_effect" = c(sort(unique(precomputed_data$subject_effect)), NA),
+  "mu0" = c(sort(unique(precomputed_data$mu0)), NA),
+  "dispersion" = c(sort(unique(precomputed_data$dispersion)), NA),
+  "nb_cell_per_sample" = c(sort(unique(precomputed_data$nb_cell_per_sample)), NA)
 )
 
 #' Shiny app
 #'
 #' Interactive shiny app to predict the power of a CyTOF experiment.
 #'
-#' @return Shiny app.
+#' @return Interactive shiny app.
 #' @examples
 #' # Launch the shiny app
 #' if (interactive()) {
